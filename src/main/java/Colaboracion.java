a3e74863656182c34bb56f5b186c68402c7a4d2e
Miriam Calvo <miriam.calvo3@educa.madrid.org>
2026-01-29 11:37:03 +0100

    Añadir menasaje desde Ubuntu

--- src/main/java/Colaboracion.java
+++ src/main/java/Colaboracion.java
@@ -1,5 +1,6 @@
 public class Colaboracion {
     public static void main(String[] args) {
         System.out.println("Versión inical del proyecto.");
+	Sysyem.out.println("Mensaje desde Ubuntu.");
     }
 }
