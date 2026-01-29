9a071779fa9a5f58581c2bef12f9db91d7f4658b
miriam-cf <miriam.calvo3@educa.madrid.org>
2026-01-29 11:29:50 +0100

    Cambios realizados a Colaboracion en Windows

--- src/main/java/Colaboracion.java
+++ src/main/java/Colaboracion.java
@@ -1,5 +1,6 @@
 public class Colaboracion {
     public static void main(String[] args) {
         System.out.println("Versión inical del proyecto.");
+        System.out.println("Mensaje desde Windows.");
     }
 }
